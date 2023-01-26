c6f19ef3973b27e3afa8a27df7abcf470ee24689
wahyupambudi <wahyupambudi823@gmail.com>
2023-01-26 19:06:09 +0700

    master: add frontend reactjs

---
+++ src/components/FormEditUser.jsx
@@ -0,0 +1,179 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
+const FormEditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [token, setToken] = useState("");
+  const [expire, setExpire] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const RefreshToken = async () => {
+    try {
+      const response = await axios.get("http://localhost:2023/token");
+      setToken(response.data.accessToken);
+      const decoded = jwt_decode(response.data.accessToken);
+      setExpire(decoded.exp);
+    } catch (error) {
+      if (error.response) {
+        // history.push("/");
+      }
+    }
+  };
+
+  useEffect(() => {
+    RefreshToken();
+
+    const axiosJWT = axios.create();
+
+    axiosJWT.interceptors.request.use(
+      async (config) => {
+        const currentDate = new Date();
+        if (expire * 1000 < currentDate.getTime()) {
+          const response = await axios.get("http://localhost:2023/token");
+          config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+          setToken(response.data.accessToken);
+          const decoded = jwt_decode(response.data.accessToken);
+          setExpire(decoded.exp);
+        }
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+
+    const getUserById = async () => {
+      try {
+        const response = await axiosJWT.get(
+          `http://localhost:2023/users/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setName(response.response.data.name);
+        setEmail(response.response.data.email);
+        setRole(response.response.data.role);
+      } catch (error) {
+        if (error.response) {
+          setMsg(error.response.data.msg);
+          console.log(setMsg);
+        }
+      }
+    };
+    getUserById();
+  }, [id]);
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.patch(`http://localhost:2023/users/${id}`, {
+        name: name,
+        email: email,
+        password: password,
+        confPassword: confPassword,
+        role: role,
+      });
+      navigate("/users");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
+  return (
+    <div>
+      <h1 className="title">Users</h1>
+      <h2 className="subtitle">Update User</h2>
+      <div className="card is-shadowless">
+        <div className="card-content">
+          <div className="content">
+            <form onSubmit={updateUser}>
+              <p className="has-text-centered">{msg}</p>
+              <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Name"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                  <input
+                    type="password"
+                    className="input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="******"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Confirm Password</label>
+                <div className="control">
+                  <input
+                    type="password"
+                    className="input"
+                    value={confPassword}
+                    onChange={(e) => setConfPassword(e.target.value)}
+                    placeholder="******"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Role</label>
+                <div className="control">
+                  <div className="select is-fullwidth">
+                    <select
+                      value={role}
+                      onChange={(e) => setRole(e.target.value)}
+                    >
+                      <option value="admin">Admin</option>
+                      <option value="user">User</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <button type="submit" className="button is-success">
+                    Update
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormEditUser;
